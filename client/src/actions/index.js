0c8faf0f9a174e3d37ac5ae28337aa22ba67c751
Patrick Sheriddan <sheridp2@gmail.com>
2020-11-24 12:08:10 -0800

    stream edit now pull id param from url and fetches stream with that id

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,6 @@
 import streams from '../apis/streams'
 import { SIGN_IN, SIGN_OUT, CREATE_STREAM, FETCH_STREAMS, FETCH_STREAM, DELETE_STREAM, EDIT_STREAM} from './types'
-
+import history from '../history'
 export const signIn = (userId) => {
   return {
     type: SIGN_IN,
@@ -19,6 +19,7 @@ export const createStream = formValues => async (dispatch, getState)=> {
   const response = await streams.post('./streams', { ...formValues, userId })
 
   dispatch({ type: CREATE_STREAM, payload: response.data})
+  history.push('/')
 }
 
 export const fetchStreams = () => async dispatch => {
